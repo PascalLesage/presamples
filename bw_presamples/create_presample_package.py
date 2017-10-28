ef7304f8a0d719b6590092055cee68b36978c907
PascalLesage <pascal.lesage@polymtl.ca>
2017-10-28 00:31:53 -0400

    Untested first pass at presample creation, loading and utils
    Taken and modified form past projects. UNTESTED.
    Can be used for any of Inventory elements, cfs and parameters. No obligation for any of these to be in presample.

---
+++ bw_presamples/create_presample_package.py
@@ -0,0 +1,238 @@
+from __future__ import print_function, unicode_literals
+from eight import *
+
+from . import projects,mapping
+from .filesystem import md5
+from .utils import MAX_INT_32, TYPE_DICTIONARY, numpy_string
+import json
+import numpy as np
+import os
+import shutil
+import uuid
+
+
+def create_presample_package(inventory_elements=None, inventory_elements_samples=None, inventory_mapped=False, inventory_dtype="float32",
+                             cfs=None, cfs_samples=None, cfs_mapped=False, cfs_dtype="float32",
+                             parameters=None, parameters_samples=None, parameters_dtype="float32",
+                             id_=None, overwrite=False):
+    """Create a new subdirectory in the ``project`` folder that stores presampled values.
+
+    The presamples values can be for the following types of objects:
+        * Elements of the inventory matrices (A or B matrices)
+        * Characterization factors (i.e. elements of the C matrix)
+        * Exogenous parameters
+    
+    The presample directory necessarily contains the following file:
+    * ``datapackage.json``: Contains metadata about the other files, including integrity checks. 
+    
+    If the presamples package is to contain sampled values for elements of the inventory matrices,
+    the presamples directory necessarily holds the following two additional files:
+    * ``{id_}.inventory_elements.npy``: Information to locate the presampled values in the A and B matrices.
+    * ``{id_}.inventory_elements_samples.npy``: Array of the presampled values for the corresponding inventory elements.
+    
+    If the presamples package is to contain sampled values for characterization factors,
+    the presamples directory necessarily holds the following two additional files:
+    * ``{id_}.cfs.npy``: Information to locate the presampled values in the C matrix.
+    * ``{id_}.cfs_samples.npy``: Array of the presampled values for the corresponding characterization factors
+
+    If the presamples package is to contain sampled values for exogenous parameters,
+    the presamples directory necessarily holds the following two additional files:
+    * ``{id_}.parameters.json``: List of exogenous parameters.
+    * ``{id_}.parameters_samples.npy``: Array of the presampled values for exogenous parameters.
+
+    The arguments that are passed depend on the type of values the presamples package should hold. 
+
+    If the presamples package is to contain sampled values for elements of the A or B matrices,
+    the following arguments are obligatory:
+    * ``inventory_elements``: 
+            The format is (input activity, output activity, string label for exchange type), 
+            e.g. ``[("my database", "an input"), ("my database", "an output"), "technosphere"]``.
+    * ``elements_samples``:
+            Array with sampled values for the elements. Columns represent dependent iterations,
+            each row relates to the corresponding row in ``inventory_elements``
+    * ``inventory_mapped``: Are the arrays in ``inventory_elements`` already mapped to integers using ``mapping``. Boolean, default ``False``.
+    
+    If the presamples package is to contain sampled values for characterization factors,
+    the following arguments are obligatory:
+    * ``cfs``: 
+            The format is a list of flow identifiers, e.g. ``[('biosphere3', 'f9055607-c571-4903-85a8-7c20e3790c43')]``.
+    * ``cfs_samples``:
+            Array with sampled values for the elements. Columns represent dependent iterations,
+            each row relates to the corresponding row in ``cfs``
+    * ``cfs_mapped``: Are the arrays in ``cfs`` already mapped to integers using ``mapping``. Boolean, default ``False``.
+
+    If the presamples package is to contain sampled values for exogenous parameters,
+    the following arguments are obligatory:
+    * ``parameters``: A list containing information on exogenous parameters
+    * ``parameters_samples``: Array with sampled values for exogenous parameters
+    
+    The following arguments are optional:
+    * ``id_``: Unique id for this collection of presamples. Optional, generated automatically if not set.
+    * ``overwrite``: If True, replace an existing presamples package with the same ``_id`` if it exists. Default ``False``
+
+    Returns ``id_`` and the absolute path of the created directory.
+
+    """
+
+    # Common to all types of stored presamples
+    if id_ is None:
+        id_ = uuid.uuid4().hex
+    
+    base_dir = os.path.join(projects.request_directory('presamples'), id_)
+    if os.path.isdir(base_dir):
+        if not overwrite:
+            raise ValueError("This presampled directory already exists")
+        else:
+            shutil.rmtree(base_dir)
+
+    os.mkdir(base_dir)
+    datapackage = {
+        "id": id_,
+        "profile": "data-package",
+        "content": [],
+        "resources": []
+        }
+
+    
+
+    # inventory_elements
+    if inventory_elements is not None:
+        assert inventory_elements_samples, "Cannot process inventory elements, missing information"
+        assert isinstance(inventory_elements_samples, np.ndarray), "The inventory elements samples should be in a numpy array"
+        assert len(inventory_elements)==inventory_elements_samples.shape[0], \
+            "The number of inventory elements does not correspond to the number of inventory element samples"
+
+        inv_dtype = [
+            (numpy_string('input'), np.uint32),
+            (numpy_string('output'), np.uint32),
+            (numpy_string('row'), np.uint32),
+            (numpy_string('col'), np.uint32),
+            (numpy_string('type'), np.uint8),
+        ]
+
+        inventory_elements_arr = np.zeros(len(inventory_elements), dtype=inv_dtype)
+
+        _ = lambda x: x if mapped else mapping[x]
+
+        for i, row in enumerate(inventory_elements):
+            processed = (_(row[0]), _(row[1]), MAX_INT_32, MAX_INT_32, TYPE_DICTIONARY[row[2]])
+            inventory_elements_arr[i] = processed
+
+        inventory_elements_fp = os.path.join(base_dir, "{}.inventory_elements.npy".format(id_))
+        inventory_elements_samples_fp = os.path.join(base_dir, "{}.inventory_elements_samples.npy".format(id_))
+        
+        np.save(inventory_elements_fp, inventory_elements_arr, allow_pickle=False)
+        np.save(inventory_elements_samples_fp, inventory_elements_samples.astype(np.float32), allow_pickle=False)
+
+        datapackage['content'].append('inventory_elements')
+        datapackage['resources'].extend(
+            [{
+                "name": "inventory_elements_samples",
+                "path": "{}.inventory_elements_samples.npy".format(id_),
+                "profile": "data-resource",
+                "format": "npy",
+                "mediatype": "application/octet-stream",
+                "hash": md5(inventory_elements_samples_fp),
+                "dtype": inventory_dtype,
+                "shape": inventory_elements_samples.shape,
+            },
+            {
+                "name": "inventory_elements",
+                "path": "{}.inventory_elements.npy".format(id_),
+                "profile": "data-resource",
+                "format": "npy",
+                "mediatype": "application/octet-stream",
+                "hash": md5(inventory_elements_fp),
+            }
+            ])
+
+    # cfs
+    if cfs is not None:
+        assert cfs_samples, "Cannot process cfs, missing information"
+        assert isinstance(cfs_samples, np.ndarray), "The cfs samples should be in a numpy array"
+        assert len(cfs)==cfs_samples.shape[0], \
+            "The number of cfs does not correspond to the number of cf samples"
+
+        ia_dtype = [
+            (numpy_string('flow'), np.uint32),
+            (numpy_string('row'), np.uint32),
+        ]
+
+        cfs_arr = np.zeros(len(cfs), dtype=ia_dtype)
+
+        _ = lambda x: x if mapped else mapping[x]
+
+        for i, row in enumerate(cfs):
+            processed = (_(row), MAX_INT_32)
+            cfs_arr[i] = processed
+
+        cfs_fp = os.path.join(base_dir, "{}.cfs.npy".format(id_))
+        cfs_samples_fp = os.path.join(base_dir, "{}.cfs_samples.npy".format(id_))
+        
+        np.save(cfs_fp, cfs, allow_pickle=False)
+        np.save(cfs_samples_fp, cfs_samples.astype(np.float32), allow_pickle=False)
+
+        datapackage['content'].append('cfs')
+        datapackage['resources'].extend(
+            [{
+                "name": "cfs_samples",
+                "path": "{}.cfs_samples.npy".format(id_),
+                "profile": "data-resource",
+                "format": "npy",
+                "mediatype": "application/octet-stream",
+                "hash": md5(cfs_samples_fp),
+                "dtype": cfs_dtype,
+                "shape": cfs_samples_fp.shape,
+            },
+            {
+                "name": "cfs",
+                "path": "{}.cfs.npy".format(id_),
+                "profile": "data-resource",
+                "format": "npy",
+                "mediatype": "application/octet-stream",
+                "hash": md5(cfs_fp),
+            }
+            ])
+                
+    if parameters is not None:
+        assert parameters_samples, "Exogenous parameters were passed, but parameter samples are missing."
+        assert len(parameters)==parameters_samples.shape[0], "The number of exogenous parameters does not correspond to the number of samples"
+        assert isinstance(parameters_samples, np.ndarray), "The exogenous parameters samples should be a numpy array"
+        if elements is not None:
+            assert elements_samples.shape[1]==parameters_samples.shape[1], 'Number of iterations differs for elements and exogenous parameters: invalid samples'
+
+        parameters_samples_fp = os.path.join(base_dir, "{}.parameters_samples.npy".format(id_))
+        parameters_fp = os.path.join(base_dir, "{}.parameters.json".format(id_))
+        
+        with open(parameters_fp, "w") as f:
+            json.dump(parameters, f)
+        np.save(parameters_samples_fp, parameters_samples, allow_pickle=False)
+        
+        datapackage['content'].append('parameters')
+        datapackage['resources'].extend(
+            [
+            {
+                "name": "parameters_samples",
+                "path": "{}.parameters_samples.npy".format(id_),
+                "profile": "data-resource",
+                "format": "npy",
+                "mediatype": "application/octet-stream",
+                "hash": md5(parameters_samples_fp),
+                "dtype": parameters_dtype,
+                "shape": parameters_samples.shape            
+            }, {
+                "name": "parameters",
+                "path": "{}.parameters.json".format(id_),
+                "profile": "data-resource",
+                "format": "json",
+                "mediatype": "application/json",
+                "hash": md5(parameters_fp),
+            }
+            ])
+
+    with open(os.path.join(base_dir, "datapackage.json"), "w") as f:
+        json.dump(datapackage, f)
+
+    return id_, base_dir
+
+
---
+++ bw_presamples/presampling_utils.py
@@ -0,0 +1,185 @@
+""" Set of functions that generate presample arrays from inventory parameters
+    or that assist in doing so.
+"""
+
+from bw2data import databases, Database, get_activity
+from bw2data.backends.peewee.proxies import Activity, Exchange
+from stats_arrays import uncertainty_choices, UncertaintyBase
+import numpy as np
+
+def get_exchange(param):
+    """ Temporary function that returns an exchange based on input and output keys and type.
+    Will not return anything if there is ambiguity.
+    Eventually needs to be replaced with something more robust
+    Param defined as tuple of input activity, output activity, string label for exchange type (one of technosphere, biosphere or production)
+    """
+    try:
+        act = get_activity(param[1])
+    except:
+        print("Output for exchange {} not found".format(param))
+        return None
+    if param[2] == 'technosphere':
+        candidates_for_exc = [exc for exc in act.technosphere() if exc.input == param[0]]
+    elif param[2] == 'biosphere':
+        candidates_for_exc = [exc for exc in act.biosphere() if exc.input == param[0]]
+    elif param[2] == 'production':
+        candidates_for_exc = [exc for exc in act.production() if exc.input == param[0]]
+    else:
+        print("Exchange type for {} not well defined: no such exchange type {}".format(param, param[2]))
+        return None        
+    if len(candidates_for_exc) == 0:
+        print("Exchange {} not found".format(exc_approx_id))
+        return None
+    elif len(candidates_for_exc) > 1:
+        print("Exchange {} ambiguous (more than one exchange found)".format(exc_approx_id))
+        return None
+    else: # If the exchange is found:
+        return candidates_for_exc[0]
+
+def convert_exchange_to_param(exc):
+    """Transforms a bw2data.backends.peewee.proxies.Exchange into a param (input['key'], output['key'], type)"""
+    return (exc.input.key, exc.output.key, exc['type'])
+
+def get_technosphere_input_params_with_shared_unit(act_key, unit,  as_params=True):
+    """ Returns all technosphere inputs with a specified unit
+    If ``as_params``, the params, as (input.key, output.key, type) are returned,
+    else the actual exchanges are returned"""
+    act = get_activity(act_key)
+    exchanges = [exc for exc in act.technosphere() if exc['unit']==unit]
+    if as_params:
+        return [convert_exchange_to_param(exc) for exc in exchanges]
+    else:
+        return exchanges
+
+def sum_amounts_of_exchanges(list_exchanges):
+    """ Returns the sum of statiic amounts of a set of exchanges.
+    Will refuse to cooperate if the exchanges do not have the same unit.
+    TODO: Could be refined to convert units (e.g. MJ to kWh to sum heat and electricity)
+    """
+    assert isinstance(list_exchanges, list), "list_exchanges should be a list"
+    assert len(set([exc['unit'] for exc in list_exchanges]))==1, "Exchanges do not all have the same unit" 
+    total = 0
+    for exc in list_exchanges:
+        total += exc['amount']
+    return total
+
+def get_rough_samples(exchange_list, iterations):
+    """Helper function to generate samples for set of exchanges. 
+    Rough samples are postprocessed according to different rules in other functions"""
+    samples_arr = np.empty([len(exchange_list), iterations])
+    # Populate array and positions_df
+    for i, exc in enumerate(exchange_list):
+        samples_arr[i, :] = exc.random_sample(iterations)
+    return samples_arr
+	
+
+def inputs_sum_to_fixed_amount_sample(params, expected_sum='total_static', hold_certain_values_constant=True, iterations=1000):
+    """Return a sample array where a set of technosphere exchanges sum to a predetermined amount.
+    Useful only in cases where all exchanges are either input or output.
+    The fixed sum is ensured by a post hoc rescaling of sampled values, for each iteration
+
+    Input arguments:
+    * ``params``: The parameters representing the technosphere exchanges that need to be rescaled
+    * ``expected_sum``: The amount all exchanges should add to when rescaling
+    * ``hold_certain_values_constant``: If True, exchanges with no uncertainty (uncertainty_type in (0, 1)) are not rescaled
+    * ``iterations``: Number of iterations to generate
+
+    Note: there is no way for now to unambiguously identify an exchange. If there is ambiguity, the 
+    function will not return anything.
+    """    
+    # Ensure technosphere inputs all refer to the same activity
+    assert len(set([param[1] for param in params]))==1, "All inputs should be for the same activity"
+    # Ensure all params are for technosphere inputs
+    assert all([param[2]=='technosphere' for param in params]), "All params should refer to technosphere inputs"
+    # Get exchange proxy objects:
+    exc_list = [get_exchange(param) for param in params]
+    # Ensure exc_list does not contain any None
+    if any([exc is None for exc in exc_list]):
+        print("Could not find all exchanges, no sample generated")
+        return None
+    # Get expected sum
+    if expected_sum == 'total_static':
+        expected_sum = sum_amounts_of_exchages(exc_list)
+
+    # Get rough samples
+    samples_arr = get_rough_samples(exc_list, iterations)    
+
+    if hold_certain_values_constant == True:
+        # Create collector for exchanges with no uncertainty:
+        fixed_indices = []
+        for param, exc in zip(params, exc_list):
+            if not hasattr(exc, 'uncertainty') or exc.uncertainty['uncertainty type'] in [0, 1]:
+                fixed_indices.append(params.index(param))
+        if fixed_indices:
+            non_fixed_indices = list(set([*range(len(params))]) - set(fixed_indices))        
+            for i in range(iterations):
+                scaling = expected_sum-samples_arr[fixed_indices, i].sum()/samples_arr[non_fixed_indices, i].sum()
+                samples_arr[non_fixed_indices, i] = scaling * samples_arr[non_fixed_indices, i]
+        else:
+            hold_certain_values_constant = False # Distinction no longer relevant
+    if hold_certain_values_constant == False:
+        for i in range(iterations):
+            scaling = expected_sum/samples_arr[:, i].sum()
+            samples_arr[:, i] = scaling * samples_arr[:, i]
+    return samples_arr
+		
+	
+def kronecker_delta_selector(params, negative=False, use_amounts=True, iterations=1000, also_return_random_var=False):
+    """Return a sample array where only one exchange amount is not 0, based on the relative probability of occurence of exchanges
+    
+    Input arguments:
+    * ``params``: The parameters representing the exchanges that will be set to 0 or another value for each iteration
+    * ``Negative``: If True, the non-zero amounts are negative
+    * ``Use amounts``: If True, the non-zero amounts are set to the initial exchange amount. If False, the amounts are 1 or -1
+    * ``Iterations``: Number of iterations
+    * ``also_return_random_var``: The criteria used to determine whether an exchange amount is 0 or not uses a random sampling between 0 and 1. 
+        If this value is True, the sampled values for this variable are also returned. This can be useful for
+        sensitivity or contribution to variance analyses
+    Note: there is no way for now to unambiguously identify an exchange from params. If there is ambiguity, the 
+    function will not return anything.
+    Note: The function only makes sense if functionaly equivalent exchanges are randomly selected. This entails that 
+    they should all be of the same type (e.g. all technosphere inputs, all emissions, etc.) If they are not,
+    the function will not return anything.
+    """
+    # ensure all exchanges are of the same type
+    assert [param[2] for param in params].count(params[0][2]) == len(params), "All exchanges must be of the same type"
+    
+    exc_list = [get_exchange(param) for param in params]
+    # Ensure exc_list does not contain any None
+    if any([exc is None for exc in exc_list]):
+        print("Could not find all exchanges, no sample generated")
+        return None
+    
+    # Get rough samples
+    samples_arr = get_rough_samples(exc_list, iterations)    
+    
+    # Norm samples so that they will be between 0 and 1
+    normed = samples_arr/samples_arr.sum(axis=0)
+    # Cummulative sum for each iteration
+    cum = np.cumsum(normed, axis=0)
+    # Add a zero row
+    cum_w_0 = np.concatenate((np.zeros((1, cum.shape[1])), cum), axis=0)
+    # Randomly generate values between 0 and 1
+    rand = np.random.random(cum_w_0.shape[1])
+    # 1 if random value is in range defined by cum_w_0[i-1, i], 0 otherwise
+    probs = np.transpose(np.array(
+                [[cum_w_0[i-1, j] <= rand[j] < cum_w_0[i, j]
+                    for i in np.arange(1, cum_w_0.shape[0])]
+                    for j in np.arange(cum_w_0.shape[1])],
+                dtype=int))
+    # Change sign as necessary:
+    if negative:
+        probs = np.absolute(probs) * -1
+    
+    if not use_amounts:
+        if also_return_random_var:
+            return probs, rand
+        else:
+            return probs
+    else:
+        scaling = np.array([exc['amount'] for exc in exc_list])
+        if also_return_random_var:
+            return np.dot(np.diag(scaling), probs), rand
+        else:
+            return np.dot(np.diag(scaling), probs)  
+        

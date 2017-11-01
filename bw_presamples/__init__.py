e2d16b6cdd2f1517680f412ac055d7a2939255d0
PascalLesage <pascal.lesage@polymtl.ca>
2017-11-01 13:54:09 -0400

    Corrected load filepath for inventory elements

--- bw_presamples/__init__.py
+++ bw_presamples/__init__.py
@@ -2,7 +2,12 @@
 
 __all__ = [
     'create_presamples_package',
-    'Presamples'
+    'Presamples',
+    'get_exchange',
+    'convert_exchange_to_param',
+	'get_technosphere_input_params_with_shared_unit',
+	'inputs_sum_to_fixed_amount_sample',
+	'kronecker_delta_selector',
 ]
 
 
@@ -10,3 +15,11 @@ __version__ = (0, 0, 'dev')
 
 from .create_presamples_package import create_presamples_package
 from .presamples_loading import Presamples
+from .utils import (
+	get_exchange,
+	convert_exchange_to_param,
+	get_technosphere_input_params_with_shared_unit,
+	inputs_sum_to_fixed_amount_sample,
+	kronecker_delta_selector,
+	)
+	

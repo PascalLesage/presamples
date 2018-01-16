eab356e631f88e1504c1efcdbc630c0bd371b764
Chris Mutel <cmutel@gmail.com>
2018-01-16 22:55:21 +0100

    Fix import errors

--- bw_presamples/__init__.py
+++ bw_presamples/__init__.py
@@ -2,44 +2,44 @@ __all__ = [
     'create_presamples_package',
     'get_exchange',
     'convert_exchange_to_param',
-	'get_technosphere_input_params_with_shared_unit',
-	'convert_parameter_set_dict_to_presample_package',
-	'inputs_sum_to_fixed_amount_sample',
-	'kronecker_delta_selector',
-	'Campaign',
-	'load_campaign_from_registry',
-	'list_campaigns',
-	'create_set_of_presamples_packages_for_agg_data',
-	'list_agg_presamples_in_lca'
+    'get_technosphere_input_params_with_shared_unit',
+    'convert_parameter_set_dict_to_presample_package',
+    'inputs_sum_to_fixed_amount_sample',
+    'kronecker_delta_selector',
+    'Campaign',
+    'load_campaign_from_registry',
+    'list_campaigns',
+    'create_set_of_presamples_packages_for_agg_data',
+    'list_agg_presamples_in_lca',
     'FORMATTERS',
     'IrregularPresamplesArray',
     'MatrixPresamples',
-	'Campaign'
+    'Campaign',
 ]
 
 
 __version__ = (0, 0, 'dev')
 
 from .create_presamples_package import (
-	create_presamples_package, 
-	convert_parameter_set_dict_to_presample_package
-	)
+    create_presamples_package, 
+    convert_parameter_set_dict_to_presample_package
+    )
 from .utils import (
-	get_exchange,
-	convert_exchange_to_param,
-	get_technosphere_input_params_with_shared_unit,
-	inputs_sum_to_fixed_amount_sample,
-	kronecker_delta_selector,
-	)
+    get_exchange,
+    convert_exchange_to_param,
+    get_technosphere_input_params_with_shared_unit,
+    inputs_sum_to_fixed_amount_sample,
+    kronecker_delta_selector,
+    )
 from .campaigns import (
-	Campaign, 
-	load_campaign_from_registry,
-	list_campaigns
-	)	
+    Campaign, 
+    load_campaign_from_registry,
+    list_campaigns
+    )   
 from .presamples_for_single_indicator_agg_acts import(
-	create_set_of_presamples_packages_for_agg_data,
-	list_agg_presamples_in_lca,
-	)
+    create_set_of_presamples_packages_for_agg_data,
+    list_agg_presamples_in_lca,
+    )
 from .array import IrregularPresamplesArray
 from .packaging import create_presamples_package, FORMATTERS
 from .matrix_presamples import MatrixPresamples
